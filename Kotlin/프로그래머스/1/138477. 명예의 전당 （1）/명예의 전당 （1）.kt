8e4e6d7d543d1e055e80fa9123148c4f9033dc6b
neoneoneo123 <128209823+neoneoneo123@users.noreply.github.com>
2024-03-07 11:37:53 +0900

    [level 1] Title: 명예의 전당 (1), Time: 20.24 ms, Memory: 63.8 MB -BaekjoonHub

--- Kotlin/프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.kt
+++ Kotlin/프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.kt
@@ -1,14 +1,12 @@
 class Solution {
     fun solution(k: Int, score: IntArray): IntArray {
         var answer: IntArray = intArrayOf()
-        var hof: IntArray = intArrayOf()
-                
+        var hof: IntArray = intArrayOf()       
         score.forEach {
             hof += it
             var tempHof: IntArray = hof.sortedDescending().take(k).toIntArray()
            answer += tempHof.minOrNull()!!
-        }
-              
+        }     
         return answer
     }
 }
