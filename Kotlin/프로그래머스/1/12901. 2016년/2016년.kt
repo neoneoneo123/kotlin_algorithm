bf5efa7261ed953e3e5cbad6c1354dee5234d93c
neoneoneo123 <128209823+neoneoneo123@users.noreply.github.com>
2024-03-08 09:59:32 +0900

    [level 1] Title: 2016년, Time: 3.37 ms, Memory: 60 MB -BaekjoonHub

--- Kotlin/프로그래머스/1/12901. 2016년/2016년.kt
+++ Kotlin/프로그래머스/1/12901. 2016년/2016년.kt
@@ -1,25 +1,19 @@
 class Solution {
     fun solution(a: Int, b: Int): String {
-        var answer = ""
         var daysMap: Map<Int, String> = mapOf (
             0 to "SAT", 1 to "SUN", 2 to "MON", 3 to "TUE",
             4 to "WED", 5 to "THU", 6 to "FRI"
-        ) 
+        )
         
-        var h = 0
-        var date = b
-        var month = a
-        var year = 2016
-        
-        if (month < 3) {
-            month += 12
-            year--
+        var m = a
+        var y = 2016
+        if (m < 3) {
+            m += 12
+            y--
         }
         
-        h = (date + (13 * (month + 1) / 5) + year + (year / 4) - (year / 100) + (year / 400)) % 7
-                
-        answer = daysMap[h].toString()
-        
-        return answer
+        return daysMap[(b + (13 * (m + 1) / 5) + y + (y / 4) - (y / 100) + (y / 400)) % 7].toString()
     }
 }
+
+//https://en.wikipedia.org/wiki/Zeller%27s_congruence
--- Kotlin/프로그래머스/1/12901. 2016년/README.md
+++ Kotlin/프로그래머스/1/12901. 2016년/README.md
@@ -1,10 +1,10 @@
 # [level 1] 2016년 - 12901 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901#) 
 
 ### 성능 요약
 
-메모리: 60.5 MB, 시간: 2.63 ms
+메모리: 60 MB, 시간: 3.37 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 08일 09:39:39
+2024년 03월 08일 09:59:30
 
 ### 문제 설명
 
