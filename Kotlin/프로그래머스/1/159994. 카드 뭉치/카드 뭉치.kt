c15e0abad629b5ed50cefe8c10bce7fcfc1f8cc3
neoneoneo123 <128209823+neoneoneo123@users.noreply.github.com>
2024-03-09 11:55:57 +0900

    [level 1] Title: 카드 뭉치, Time: 19.59 ms, Memory: 63 MB -BaekjoonHub

--- Kotlin/프로그래머스/1/159994. 카드 뭉치/카드 뭉치.kt
+++ Kotlin/프로그래머스/1/159994. 카드 뭉치/카드 뭉치.kt
@@ -2,37 +2,23 @@ import java.util.Stack
 
 class Solution {
     fun solution(cards1: Array<String>, cards2: Array<String>, goal: Array<String>): String {
-        var answer: String = ""
-        var goalCheck: MutableList<String> = goal.toMutableList()
-        var sortedCards1 = cards1.reversed()
-        var sortedCards2 = cards2.reversed()
-        var cardStack1 = Stack<String>()
-        var cardStack2 = Stack<String>()
-        
-        sortedCards1.forEach {
-            cardStack1.push(it)
-        }
-        sortedCards2.forEach {
-            cardStack2.push(it)
-        }
+        var cardStack1 = Stack<String>().apply { cards1.reversed().forEach { push(it) } }
+        var cardStack2 = Stack<String>().apply { cards2.reversed().forEach { push(it) } }
+        val goalCheck = ArrayList<String>(goal.toList())
         
         goal.forEach {
-            if (cardStack1.isNotEmpty() && it == cardStack1.peek()) {
-                goalCheck.remove(cardStack1.peek())
-                cardStack1.pop()
-                
-            } else if (cardStack2.isNotEmpty() && it == cardStack2.peek()) {
-                goalCheck.remove(cardStack2.peek())
-                cardStack2.pop()
+            when {
+                cardStack1.isNotEmpty() && it == cardStack1.peek() -> {
+                    goalCheck.remove(cardStack1.peek())
+                    cardStack1.pop()
+                }
+                cardStack2.isNotEmpty() && it == cardStack2.peek() -> {
+                    goalCheck.remove(cardStack2.peek())
+                    cardStack2.pop()
+                }
             }
         }
-        
-        if (goalCheck.isEmpty()) {
-            answer += "Yes"
-        } else {
-            answer += "No"
-        }
-              
-        return answer
+                
+        return if (goalCheck.isEmpty()) "Yes" else "No"
     }
 }
