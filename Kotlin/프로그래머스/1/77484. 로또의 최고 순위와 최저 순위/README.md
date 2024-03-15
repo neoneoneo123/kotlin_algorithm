e08fbee5244403e71163afcbf78dd4f2214ca19b
neoneoneo123 <128209823+neoneoneo123@users.noreply.github.com>
2024-03-15 10:40:22 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.02 ms, Memory: 61.2 MB -BaekjoonHub

--- Kotlin/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
+++ Kotlin/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
@@ -1,10 +1,10 @@
 # [level 1] 로또의 최고 순위와 최저 순위 - 77484 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484#) 
 
 ### 성능 요약
 
-메모리: 63 MB, 시간: 13.67 ms
+메모리: 61.2 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 15일 09:43:32
+2024년 03월 15일 10:40:20
 
 ### 문제 설명
 
--- Kotlin/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.kt
+++ Kotlin/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.kt
@@ -1,36 +1,29 @@
+/*
+최저 : 0 제외한 것들 중 매칭 되는 개수로 판단
+최고 : 최저 등수와 0 개수로 판단
+
+1 2 3 4 5 6
+0 0 3 4 5 6 -> 최저 : 4개는 매칭(3등)
+            -> 최고 : 0 자리가 1, 2일 경우 6개 매칭(1등)
+*/
+
 class Solution {
     fun solution(lottos: IntArray, win_nums: IntArray): IntArray {
-        var answer: IntArray = intArrayOf()
-        var muWinNums = win_nums.toMutableList()
-        var lowest = 7
-        var highest = 7
-        var zeros = 0
-        
-        //최저등수
+        var zeros = 0; var lowest = 7; var highest = 7      
         lottos.forEach { lotto ->
             win_nums.forEach { winNum ->
                 if (lotto == winNum) {
                     lowest--
-                    muWinNums.remove(lotto)
                 }
             }
-            if (lotto == 0) {
-                zeros++
-            }
+            if (lotto == 0) zeros++
         }
                 
-        //최고등수
         highest = lowest - zeros
         
         if(highest == 7) highest--
         if(lowest == 7) lowest--
-              
-        answer += highest
-        answer += lowest
         
-        println(highest)
-        println(lowest)
-        
-        return answer
+        return intArrayOf(highest, lowest)
     }
 }
