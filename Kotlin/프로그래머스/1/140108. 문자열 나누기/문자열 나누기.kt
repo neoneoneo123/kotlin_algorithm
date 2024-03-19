8ae79e92a6c423cf78da0216ef43a563352bcbb1
neoneoneo123 <128209823+neoneoneo123@users.noreply.github.com>
2024-03-19 11:48:47 +0900

    [level 1] Title: 문자열 나누기, Time: 0.74 ms, Memory: 60.2 MB -BaekjoonHub

---
+++ Kotlin/프로그래머스/1/140108. 문자열 나누기/문자열 나누기.kt
@@ -0,0 +1,42 @@
+/*
+0. strX는 s[0], isX는 1, isNotX는 0으로 초기화한다.
+1. strX, i+1 인덱스 값을 비교한다.
+1-1. 같다면 isX에 값을 더한다.
+1-2. 다르다면 isNotX에 값을 더한다.
+
+2. isX와 isNotX를 비교한다.
+2-1. 같다면 isX, isNotX를 초기화한다.
+     다음 인덱스부터 시작하도록 i++한다.
+     strX은 i+1번째 인덱스 값으로 초기화한다.
+        
+*/
+class Solution {
+    fun solution(s: String): Int {
+        var answer: Int = 0       
+        var isX = 1
+        var isNotX = 0
+        var strX = s[0]
+        
+        var i = 0
+        while(i < s.length - 1) {
+            if (strX == s[i + 1]) isX++
+            else isNotX++
+            
+            if (isX == isNotX) {
+                answer++
+                isX = 1
+                isNotX = 0
+                i++
+                
+                if(i + 1 < s.length) strX = s[i + 1]
+            }
+            i++
+            
+            if(i == s.length - 1) answer++
+        }
+        
+        if (s.length - 1 == 0) answer++
+                
+        return answer
+    }
+}
