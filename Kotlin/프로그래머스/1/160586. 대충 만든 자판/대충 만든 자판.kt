643597844c34154414e293ec06aef7d9d792f4e6
neoneoneo123 <128209823+neoneoneo123@users.noreply.github.com>
2024-03-20 10:03:07 +0900

    [level 1] Title: 대충 만든 자판, Time: 51.39 ms, Memory: 69.7 MB -BaekjoonHub

---
+++ Kotlin/프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.kt
@@ -0,0 +1,39 @@
+/*
+1. target char를 포함하는 키가 있는가?
+    F -> -1 추가. 이번 target 끝
+    T -> 포함하는 키들의 몇 번째 인덱스에 있는가?
+        -> 더 작은 인덱스 + 1 추가    
+*/
+
+class Solution {
+    fun solution(keymap: Array<String>, targets: Array<String>): IntArray {
+        var answer: IntArray = intArrayOf()
+        
+        
+        for (i in 0 until targets.size) {
+            var pushCount = 0
+            for (j in 0 until targets[i].length) {
+                var compareCount: MutableList<Int> = mutableListOf()
+                for (k in 0 until keymap.size) {
+                    if (keymap[k].contains(targets[i][j])) {                  
+                        compareCount.add(keymap[k].indexOf(targets[i][j]))
+                    }
+                }
+                if (compareCount.size == 1) {
+                    pushCount += compareCount[0] + 1
+                    compareCount.clear()
+                } else if (compareCount.size > 1) {
+                    pushCount += compareCount.minOrNull()!! + 1
+                    compareCount.clear()
+                } else {
+                    pushCount = -1
+                    break
+                }
+            }
+            answer += pushCount
+        }
+        
+        
+        return answer
+    }
+}
