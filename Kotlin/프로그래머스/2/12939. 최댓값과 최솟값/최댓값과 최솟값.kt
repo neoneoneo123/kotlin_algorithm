bf10ee7ab52ad7d21a6e3cb06a4f2d16619b4267
neoneoneo123 <128209823+neoneoneo123@users.noreply.github.com>
2024-03-30 12:56:50 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 23.61 ms, Memory: 63.3 MB -BaekjoonHub

--- Kotlin/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.kt
+++ Kotlin/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.kt
@@ -1,71 +1,22 @@
-/*
-최솟값은 min
-최댓값은 max 쓰면 되는거 아닌가..?
-공백이 들어있어서 공백을 제외하고 생각해야함
-음수는 -를 처리해줘야함
-*/
-
 class Solution {
     fun solution(s: String): String {
-        var answer = ""
-        var numberList : MutableList<Int> = mutableListOf()
-        
+        var numberList : MutableList<Int> = mutableListOf()        
         var i = 0
-        while (i < s.length) {
-            
-            if (s[i] == ' ') {
-                continue
+        while (i < s.length) {            
+            if (s[i] == ' ') continue
+            var temp = "" + s[i]
+            var end = true
+            var j = i + 1
+            while(end) {
+                if (j < s.length) {
+                    if (s[j] == ' ') end = false
+                    else if (s[j] != ' ') temp += s[j]
+                } else end = false
+                j++
             }
-            
-            //-가 나오면 다음 공백이 나올 때까지만 잘라야함
-            var temp = ""
-            if (s[i] == '-') {                
-                temp += s[i]
-                var end = true
-                var j = i + 1
-                while(end) {
-                    if (j < s.length) {
-                        if (s[j] == ' ') {
-                            end = false
-                        } else if (s[j] != ' ') {
-                            temp += s[j]
-                        }
-                    } else {
-                        end = false
-                    }
-                    j++
-                }
-                i = j - 1
-            }
-            //-가 없으면 다음 공백이 나올 때까지만 잘라야함
-            else {
-                temp += s[i]
-                var end = true
-                var j = i + 1
-                while(end) {
-                    if (j < s.length) {
-                        if (s[j] == ' ') {
-                            end = false
-                        } else if (s[j] != ' ') {
-                            temp += s[j]
-                        }
-                    } else {
-                        end = false
-                    }
-                    j++
-                }
-                i = j - 1
-            }
-            //println(temp)
+            i = j        
             numberList += temp.toInt()
-            i++
-        }
-        
-        var min = numberList.minOrNull()!!
-        var max = numberList.maxOrNull()!!
-        
-        answer = min.toString() + " " + max.toString()
-        
-        return answer
+        }                
+        return numberList.minOrNull()!!.toString() + " " + numberList.maxOrNull()!!.toString()
     }
 }
