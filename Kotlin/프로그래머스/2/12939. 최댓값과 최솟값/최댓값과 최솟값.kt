8fd033e5603abdba3ecd3255b51c81383e740aa6
neoneoneo123 <128209823+neoneoneo123@users.noreply.github.com>
2024-03-30 13:00:43 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 22.59 ms, Memory: 61.9 MB -BaekjoonHub

--- Kotlin/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.kt
+++ Kotlin/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.kt
@@ -1,22 +1,6 @@
 class Solution {
-    fun solution(s: String): String {
-        var numberList : MutableList<Int> = mutableListOf()        
-        var i = 0
-        while (i < s.length) {            
-            if (s[i] == ' ') continue
-            var temp = "" + s[i]
-            var end = true
-            var j = i + 1
-            while(end) {
-                if (j < s.length) {
-                    if (s[j] == ' ') end = false
-                    else if (s[j] != ' ') temp += s[j]
-                } else end = false
-                j++
-            }
-            i = j        
-            numberList += temp.toInt()
-        }                
-        return numberList.minOrNull()!!.toString() + " " + numberList.maxOrNull()!!.toString()
+    fun solution(s: String): String {        
+        var list = s.split(" ").map { it.toInt() }        
+        return list.minOrNull()!!.toString() + " " + list.maxOrNull()!!.toString()
     }
 }
