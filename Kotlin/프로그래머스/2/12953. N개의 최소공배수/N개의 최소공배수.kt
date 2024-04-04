e80d4d2e648503c44ba1e14b3b29a2312b48d77d
neoneoneo123 <128209823+neoneoneo123@users.noreply.github.com>
2024-04-04 09:20:04 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.02 ms, Memory: 61 MB -BaekjoonHub

--- Kotlin/프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.kt
+++ Kotlin/프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.kt
@@ -1,22 +1,15 @@
 class Solution {
     fun solution(arr: IntArray): Int {
-        var answer = 0
-        
+        var answer = 0      
         var tempLcm = lcm(arr[0], arr[1])
         for (i in 0 until arr.size - 1) {
             answer = lcm(tempLcm, arr[i + 1])
             tempLcm = answer
-        }
-        
+        }        
         return answer
     }
     
-    fun lcm(a: Int, b: Int): Int {
-        var c = gcd(a, b)
-        return a * b / c
-    }
+    fun lcm(a: Int, b: Int): Int = a * b / gcd(a, b)
     
-    fun gcd(a: Int, b: Int): Int {
-        return if (b == 0) a else gcd(b, a % b)
-    }
+    tailrec fun gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
 }
