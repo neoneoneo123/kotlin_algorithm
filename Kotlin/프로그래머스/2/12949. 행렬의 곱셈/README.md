28155d9d77db51275b4d0718325fff69090041c6
neoneoneo123 <128209823+neoneoneo123@users.noreply.github.com>
2024-04-11 12:36:33 +0900

    [level 2] Title: 행렬의 곱셈, Time: 11.82 ms, Memory: 66.4 MB -BaekjoonHub

--- Kotlin/프로그래머스/2/12949. 행렬의 곱셈/README.md
+++ Kotlin/프로그래머스/2/12949. 행렬의 곱셈/README.md
@@ -1,10 +1,10 @@
 # [level 2] 행렬의 곱셈 - 12949 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949#) 
 
 ### 성능 요약
 
-메모리: 63.6 MB, 시간: 11.86 ms
+메모리: 66.4 MB, 시간: 11.82 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 11일 12:34:22
+2024년 04월 11일 12:36:32
 
 ### 문제 설명
 
--- Kotlin/프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.kt
+++ Kotlin/프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.kt
@@ -1,29 +1,21 @@
 class Solution {
     fun solution(arr1: Array<IntArray>, arr2: Array<IntArray>): Array<IntArray> {
-        var answer = arrayOf<IntArray>()
-        
-        //곱한 결과 행렬의 크기 m x q (m x n * p x q)
+        var answer = arrayOf<IntArray>()        
         var m = arr1.size
         var n = arr1[0].size
         var q = arr2[0].size
-        
-        //println("새로운 행렬의 크기는? : $m x $q ")
-        
+                
         for (i in 0 until m) {
             var tempArray = intArrayOf()
             for (j in 0 until q) {
-                var temp = 0
+                var sum = 0
                 for (k in 0 until n) {
-                    //println("i = $i | j = $j | k = $k")
-                    temp += arr1[i][k] * arr2[k][j]
+                    sum += arr1[i][k] * arr2[k][j]
                 }
-                //println("여기까지 하나 계산! : $temp")
-                tempArray += temp
+                tempArray += sum
             }
             answer += tempArray
-        }
-        
-        
+        }  
         return answer
     }
 }
