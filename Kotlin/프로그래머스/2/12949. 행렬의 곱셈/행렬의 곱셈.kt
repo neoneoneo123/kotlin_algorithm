7233c6a65ee0661d274f56e00e858df7f6476d01
neoneoneo123 <128209823+neoneoneo123@users.noreply.github.com>
2024-04-11 12:58:51 +0900

    [level 2] Title: 행렬의 곱셈, Time: 1.36 ms, Memory: 64.2 MB -BaekjoonHub

--- Kotlin/프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.kt
+++ Kotlin/프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.kt
@@ -1,20 +1,19 @@
 class Solution {
-    fun solution(arr1: Array<IntArray>, arr2: Array<IntArray>): Array<IntArray> {
-        var answer = arrayOf<IntArray>()        
+    fun solution(arr1: Array<IntArray>, arr2: Array<IntArray>): Array<IntArray> {      
         var m = arr1.size
         var n = arr1[0].size
         var q = arr2[0].size
+        
+        var answer = Array(m) { IntArray(q) }
                 
         for (i in 0 until m) {
-            var tempArray = intArrayOf()
             for (j in 0 until q) {
                 var sum = 0
                 for (k in 0 until n) {
                     sum += arr1[i][k] * arr2[k][j]
                 }
-                tempArray += sum
+                answer[i][j] += sum
             }
-            answer += tempArray
         }  
         return answer
     }
