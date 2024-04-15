129a2051987fffdb05f9248b880b44bb309e3df3
neoneoneo123 <128209823+neoneoneo123@users.noreply.github.com>
2024-04-15 11:23:49 +0900

    [level 2] Title: 의상, Time: 0.22 ms, Memory: 60.1 MB -BaekjoonHub

--- Kotlin/프로그래머스/2/42578. 의상/의상.kt
+++ Kotlin/프로그래머스/2/42578. 의상/의상.kt
@@ -1,25 +1,21 @@
 class Solution {
     fun solution(clothes: Array<Array<String>>): Int {
-        // 각 의상 종류별 개수를 세기 위한 HashMap
-        val clothesMap = HashMap<String, Int>()
-
-        // clothes 배열을 순회하면서 각 의상 종류의 개수 세기
+        val cMap = HashMap<String, Int>()
+        
         for (item in clothes) {
-            val itemType = item[1] // 의상 종류
-            if (clothesMap.containsKey(itemType)) {
-                clothesMap[itemType] = clothesMap[itemType]!! + 1
+            val itemType = item[1]
+            if (cMap.containsKey(itemType)) {
+                cMap[itemType] = cMap[itemType]!! + 1
             } else {
-                clothesMap[itemType] = 1
+                cMap[itemType] = 1
             }
         }
-
-        // 가능한 조합의 수 계산
-        var totalCombinations = 1
-        for (count in clothesMap.values) {
-            totalCombinations *= (count + 1) // 각 의상 종류에서 선택하지 않는 경우를 포함하여 계산
+        
+        var total = 1
+        for (count in cMap.values) {
+            total = total * (count + 1)
         }
-
-        // 모든 의상을 선택하지 않는 경우 1가지를 빼고 반환
-        return totalCombinations - 1
+        
+        return total - 1
     }
 }
